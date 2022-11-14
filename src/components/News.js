aad87de1f91435a19b711274cb590e6df6c6f742
Sagar-Patel94 <s.m.gajera.sg@gmail.com>
2022-11-14 18:38:21 +0530

    14/11/2022 Monday 06:38 PM

--- src/components/News.js
+++ src/components/News.js
@@ -2,101 +2,87 @@ import React, { Component } from "react";
 import NewsItem from "./NewsItem";
 
 export class News extends Component {
-  articles = [
-    {
-      source: {
-        id: "google-news-in",
-        name: "Google News (India)",
-      },
-      author: "Zee Media Bureau",
-      title:
-        "LIVE Updates | IND vs ENG 2nd T20 Semi-final Cricket Live Scorecard  ENG 170-0: England do a Pakista",
-      description:
-        "LIVE Updates | IND vs ENG 2nd T20 Semi-final Cricket Live Scorecard, ENG 170-0: England do a Pakista",
-      url: "https://zeenews.india.com/cricket/live-updates/live-cricket-score-ind-vs-eng-2nd-semifinal-t20-world-cup-2022-today-india-vs-england-cricket-live-score-and-updates-adelaide-oval-rohit-sharma-jos-buttler-eng-win-by-10-wickets-2532885",
-      urlToImage:
-        "https://english.cdn.zeenews.com/sites/default/files/2022/11/09/1114591-indengblog.jpg",
-      publishedAt: "2022-11-10T10:52:02+00:00",
-      content:
-        "LIVE Updates | IND vs ENG 2nd T20 Semi-final Cricket Live Scorecard, ENG 170-0: England do a Pakista",
-    },
-    {
-      source: {
-        id: "google-news-in",
-        name: "Google News (India)",
-      },
-      author: null,
-      title:
-        "Ravindra Jadeja Thanks PM After Wife Picked As Gujarat BJP Candidate",
-      description:
-        "Indian cricket star Ravindra Jadeja today congratulated wife Rivaba on being picked as a BJP candidate for the upcoming Gujarat elections and thanked Prime Minister Narendra Modi and Home Minister Amit Shah for giving her the opportunity.",
-      url: "https://www.ndtv.com/india-news/ravindra-jadeja-thanks-pm-after-wife-picked-as-gujarat-bjp-candidate-3507347",
-      urlToImage:
-        "https://c.ndtvimg.com/2022-11/55obil6_ravindra-jadeja-wife-_625x300_10_November_22.jpg",
-      publishedAt: "2022-11-10T09:08:40+00:00",
-      content:
-        "Ravindra Jadeja congratulated wife Rivaba and wished her the best for the poll contest\r\nIndian cricket star Ravindra Jadeja today congratulated wife Rivaba on being picked as a BJP candidate for the … [+1460 chars]",
-    },
-    {
-      source: {
-        id: "espn-cric-info",
-        name: "ESPN Cric Info",
-      },
-      author: null,
-      title:
-        "PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
-      description:
-        "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
-      url: "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
-      urlToImage:
-        "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
-      publishedAt: "2020-04-27T11:41:47Z",
-      content:
-        "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]",
-    },
-    {
-      source: {
-        id: "espn-cric-info",
-        name: "ESPN Cric Info",
-      },
-      author: null,
-      title:
-        "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
-      description:
-        "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
-      url: "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
-      urlToImage:
-        "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
-      publishedAt: "2020-03-30T15:26:05Z",
-      content:
-        "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]",
-    },
-  ];
   constructor() {
     super();
-    console.log("Hello i am a constructor from news component.");
     this.state = {
-        articles: this.articles
+      articles: [],
+      loading: false,
+      page: 1,
     };
   }
+
+  async componentDidMount() {
+    let url =
+      `https://newsapi.org/v2/top-headlines?country=in&apiKey=038437901b0e4d55a4af394cf7bff2ce&page=1&pageSize=${this.props.pageSize}`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    this.setState({
+      articles: parsedData.articles,
+      totalResults: parsedData.totalResults,
+    });
+  }
+
+  handlePreviousClick = async () => {
+    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=038437901b0e4d55a4af394cf7bff2ce&page=${
+      this.state.page - 1
+    }&pageSize=${this.props.pageSize}`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    this.setState({
+      page: this.state.page - 1,
+      articles: parsedData.articles,
+    });
+  };
+  handleNextClick = async () => {
+    if (this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize)) {
+    } else {
+      let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=038437901b0e4d55a4af394cf7bff2ce&page=${
+        this.state.page + 1
+      }&pageSize=${this.props.pageSize}`;
+      let data = await fetch(url);
+      let parsedData = await data.json();
+      this.setState({
+        page: this.state.page + 1,
+        articles: parsedData.articles,
+      });
+    }
+  };
+
   render() {
     return (
       <div className="container my-3">
-        <h2>NewsMonkey - Top Headlines</h2>
+        <h1 className="text-center">NewsMonkey - Top Headlines</h1>
         <div className="row">
-          <div className="col-md-4">
-            <NewsItem
-              title="myTitle"
-              discription="myDesc"
-              imageUrl="https://english.cdn.zeenews.com/sites/default/files/2022/11/09/1114591-indengblog.jpg"
-            />
-          </div>
-          <div className="col-md-4">
-            <NewsItem title="myTitle" discription="myDesc" />
-          </div>
-          <div className="col-md-4">
-            <NewsItem title="myTitle" discription="myDesc" />
-          </div>
+          {this.state.articles.map((element) => {
+            return (
+              <div className="col-md-4" key={element.url}>
+                <NewsItem
+                  title={element.title ? element.title : ""}
+                  discription={element.description ? element.description : ""}
+                  imageUrl={element.urlToImage}
+                  newsUrl={element.url}
+                />
+              </div>
+            );
+          })}
+        </div>
+        <div className="container d-flex justify-content-between">
+          <button
+            disabled={this.state.page <= 1}
+            type="button"
+            className="btn btn-dark"
+            onClick={this.handlePreviousClick}
+          >
+            &larr; Previous
+          </button>
+          <button
+          disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize)}
+            type="button"
+            className="btn btn-dark"
+            onClick={this.handleNextClick}
+          >
+            Next &rarr;
+          </button>
         </div>
       </div>
     );
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,15 +2,15 @@ import React, { Component } from "react";
 
 export class NewsItem extends Component {
   render() {
-    let { title, discription, imageUrl } = this.props;
+    let { title, discription, imageUrl, newsUrl } = this.props;
     return (
       <div className="my-3">
         <div className="card" style={{width: "18rem"}}>
-          <img src={imageUrl} className="card-img-top" alt="..." />
+          <img src={!imageUrl ? "https://www.techexplorist.com/wp-content/uploads/2022/11/gamma-ray-burst.jpg" : imageUrl} className="card-img-top" alt="..." />
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{discription}</p>
-            <a href="/newsdetail" className="btn btn-sm btn-primary">
+            <a href={newsUrl} rel="noreferrer" target="_blank" className="btn btn-sm btn-dark">
               Read More
             </a>
           </div>
